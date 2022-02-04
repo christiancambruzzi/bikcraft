b44d8d589c9c79ea709b7d9769ae65ba9d6c4264
Christian Cambruzzi <christiancambruzzi@gmail.com>
2022-02-04 16:54:17 -0300

    galeria de imagens

--- js/script.js
+++ js/script.js
@@ -19,10 +19,51 @@ const parametros = new URLSearchParams(location.search)
 
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro)
-  if(elemento){
+  if (elemento) {
     elemento.checked = true
   }
   console.log(elemento)
 }
 
 parametros.forEach(ativarProduto)
+
+// PERGUNTAS FREQUENTES
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget
+  const controls = pergunta.getAttribute('aria-controls')
+  const resposta = document.getElementById(controls)
+
+  resposta.classList.toggle('ativa')
+  const ativa = resposta.classList.contains('ativa')
+  console.log(ativa)
+  pergunta.setAttribute('aria-expanded', ativa)
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
+
+// GALERIA DE BICICLETAS
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img')
+const galeriaContainer = document.querySelector('.bicicleta-imagens')
+
+function trocarImagem(event) {
+  const img = event.currentTarget
+  const media = matchMedia('(min-width: 1000px)').matches
+  console.log(media)
+  if (media) {
+    galeriaContainer.prepend(img)
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
